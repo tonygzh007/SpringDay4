136404e9e7f4d7fbe69e6389fdd4875a0266137b
GZH <guanzh99@gmail.com>
2018-08-27 19:39:24 +0800

    init

---
+++ src/main/java/com/gzh/SpringDay4/App.java
@@ -0,0 +1,13 @@
+package com.gzh.SpringDay4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
